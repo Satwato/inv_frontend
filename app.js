a67449adf59a386149b4091a2a534c6dd574d8c7
Satwato <stwtdey1@gmail.com>
2020-02-21 02:47:03 +0530

    Sliding menu completed

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-
+var flag=0;
     const links=['./dummy1','./dummy2','./dummy3','./dummy4','./dummy5','./dummy6']
     for (link of links) {
         
-        console.log("AAAA");
+        //  console.log("AAAA");
         document.getElementById('folder').innerHTML += `<div class="col-3" id="f_outer">
         
         <a href=${link} id="f1">
@@ -33,11 +33,43 @@ span.onclick = function() {
 } 
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  // document.getElementById("main").style.marginLeft = "250px";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "45px";
-  document.getElementById("main").style.marginLeft = "45px";
+  // document.getElementById("main").style.marginLeft = "45px";
 } 
+
+   function togNav() {
+    console.log("kk");
+    if (flag==0) {
+      console.log("kk1");
+      document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
+      flag=1;
+    }
+    else {
+      console.log("kk2");
+      document.getElementById("mySidenav").style.width = "45px";
+      document.getElementById("main").style.marginLeft = "45px";
+      flag=0;
+    }
+    // $(this).data('clicked', function (_, data) { return !data; });
+}
+//   .toggle(
+//     function openNav2() {
+      
+//     document.getElementById("mySidenav").style.width = "250px";
+//     document.getElementById("main").style.marginLeft = "250px";
+//   },
+
+//   /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+//   function closeNav2() {
+//     document.getElementById("mySidenav").style.width = "0px";
+//     document.getElementById("main").style.marginLeft = "0px";
+//   } 
+// )
+
+  
